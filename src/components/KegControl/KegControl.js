6d116e66b2ce1717f81a5c1648c1dd7d7b554fde
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-07-01 12:45:54 -0700

    Create the list component

--- src/components/KegControl/KegControl.js
+++ src/components/KegControl/KegControl.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
+import KegDetails from '../KegDetails/KegDetails';
+import Edit from '../Edit/Edit';
+import NewKegForm from '../NewKegForm/NewKegForm';
 
 
-export class KegControl extends Component {
-  constructor(props){
+
+
+  export class KegControl extends Component {
+    constructor(props){
     super(props)
     this.state ={
       formVisibleOnPage :false,
@@ -61,16 +66,39 @@ export class KegControl extends Component {
       selectedKeg:null
     });
   }
-  render() {
-    let currentlyVisibleState =null;
-    let buttonText ='Hi Thre';
-    return (
+    render() {
+    
+    
+      let currentlyVisibleState =null;
+      let buttonText =null;
+      if (this.state.edit ) {      
+        currentlyVisibleState = <Edit keg = {this.state.selectedKeg} onEditKeg = {this.handleEditingKegInList} />
+        buttonText = "Return to Keg List";
+      } else if (this.state.selectedKeg != null) {
+        currentlyVisibleState = 
+        <KegDetails 
+        keg = {this.state.selectedKeg} 
+        onClickingDelete = {this.handleDeletingKeg} 
+        onClickingEdit = {this.handleEditClick} 
+        onClickingDecrement = {this.handleDecrementingPints} />
+
+        buttonText = "Return to Keg List";
+      } else if (this.state.formVisibleOnPage) {
+        currentlyVisibleState = <NewKegForm onNewKegCreation={this.handleAddingNewKegToList}  />
+
+        buttonText = "Return to Keg List";
+      } else {
+        // currentlyVisibleState = <KegList kegList={this.state.mainKegList} onKegSelection={this.handleChangingSelectedKeg} />
+      
+        // buttonText = "Add Keg";
+      }
+      return (
         <React.Fragment>
           {currentlyVisibleState}
           <button onClick={this.handleClick}> {buttonText}</button>
         </React.Fragment>
-    )
+      )
+    }
   }
-}
 
 export default KegControl
---
+++ src/components/List/List.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Keg from '../Keg.js/Keg'
+import PropTypes from 'prop-types'
+
+
+const List = (props) => {
+  return (
+    <div>
+      {props.list.map(keg=>
+        <Keg
+        whenClicked={props.onSelect}
+        name={keg.name}
+        id={keg.id}
+        key ={keg.id}
+        />)}
+    </div>
+  )
+}
+List.propTypes ={
+  List:PropTypes.array,
+  onSelect:PropTypes.func
+}
+
+export default List

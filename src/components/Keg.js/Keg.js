2c9cff012af7b212eb6141a9b5b76793a0c8e002
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-07-01 09:02:59 -0700

    set the properties of the Keg

--- src/components/Keg.js/Keg.js
+++ src/components/Keg.js/Keg.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Keg = () => {
   return (
     <div>
     </div>
   )
+}
+Keg.propTypes ={
+  name:PropTypes.string,
+  brandName:PropTypes.string,
+  alcoholContent:PropTypes.string,
+  type:PropTypes.string,
+  pints:PropTypes.number,
+  price:PropTypes.number,
+  id:PropTypes.string
 }
 
 export default Keg
---
+++ src/components/NewKegForm/NewKegForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {v4} from 'uuid'
+
+const NewKegForm = () => {
+
+  return (
+    <div>NewKegForm</div>
+  )
+}
+
+
+export default NewKegForm
